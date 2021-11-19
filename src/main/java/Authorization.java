6e07d410396a727fcfdfe59b2e8b7989deb98d2c
Verlierer <sestrunay13@gmail.com>
2021-11-19 11:39:25 +0300

    add authorization and registration methods

--- src/main/java/Authorization.java
+++ src/main/java/Authorization.java
@@ -1,14 +1,35 @@
+import manager.UserManager;
+
+import java.sql.SQLException;
+
 public class Authorization {
 
+    public final UserManager userManager = new UserManager();
 
-    public boolean login(String login, String password) {
-        //запрос к бд
+    public Authorization() throws SQLException {
+    }
+
+    public int authorizeUser(String login, String password) throws SQLException {
+        return userManager.authorizeUser(login, password);
+    }
+
+    //уточнить как быть с ошибками скл
+    public boolean registerUser(String login, String password) throws SQLException {
+        if (login.length() > 3 && password.length() >= 5) {
+            return userManager.registerUser(login, password); //изменить на хэш
+        }
         return false;
     }
 
-    public int registration(String login, String password) {
-        //добавление нового пользователя в бд
-        //возвращает user_id
-        return -1;
-    }
+
+//    public boolean login(String login, String password) {
+//        //запрос к бд
+//        return false;
+//    }
+//
+//    public int registration(String login, String password) {
+//        //добавление нового пользователя в бд
+//        //возвращает user_id
+//        return -1;
+//    }
 }
